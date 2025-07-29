9f8f5c54cc81c5bf1cba637ec43ead035757d90d
glsaacke <143575030+glsaacke@users.noreply.github.com>
2025-07-28 22:03:06 -0500

    segmented control logic

---
+++ client/src/components/ExpCard.jsx
@@ -0,0 +1,9 @@
+const ExpCard = (experience) => {
+    return ( 
+        <div className="expCard-container cbd">
+            <p>{experience.dateString}</p>
+        </div>
+     );
+}
+ 
+export default ExpCard;
--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,15 +1,45 @@
 import "../styles/Experience.css"
-import { useState } from "react";
+import experienceData from "../data/experiences.json";
+import { useState, useEffect } from "react";
+import ExpCard from "./ExpCard";
 
 const Experience = () => {
-    const [activeTab, setActiveTab] = useState("experience");
+    const [isTabExp, setIsTabExp] = useState(true);
+    const [experiences, setExperiences] = useState([])
+
+    useEffect(() => {
+        
+    }, [])
+
+    function handleSwapExperience(){
+        setIsTabExp(prev => !prev)
+    }
     
     return ( 
         <section className="experience-container">
-            <div class="segmented-control">
-                <button className="segment active" onclick="showContent('experience')">Experience</button>
-                <button class="segment" onclick="showContent('education')">Education</button>
-            </div>
+            
+                {isTabExp ? 
+                    <>
+                        <div class="segmented-control">
+                            <button className="sc-button" onClick={handleSwapExperience} style={{backgroundColor: "white", color: "#363432"}}>Experience</button>
+                            <button className="sc-button" onClick={handleSwapExperience} style={{backgroundColor: "#363432", color: "white"}}>Education</button>
+                        </div> 
+                        <div className="experience-content">
+                            {experienceData.map(exp => (
+                                <ExpCard experience={exp} key={exp.id}/>
+                            ))}
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div class="segmented-control">
+                            <button className="sc-button" onClick={handleSwapExperience} style={{backgroundColor: "#363432", color: "white"}}>Experience</button>
+                            <button className="sc-button" onClick={handleSwapExperience} style={{backgroundColor: "white", color: "#363432"}}>Education</button>
+                        </div>
+                    </>
+                }
+                
+            
         </section>
      );
 }
