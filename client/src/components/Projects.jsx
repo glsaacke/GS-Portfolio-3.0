d17e39639d8f08fe32f5e1b09019368d8f404c1d
glsaacke <glsaacke@crimson.ua.edu>
2025-07-31 23:00:56 -0500

    reactivity

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -5,9 +5,10 @@ const Projects = () => {
     return ( 
         <div className="projects-container ">
             <h2>Projects</h2>
-            <div className="project-card-container container">
+            <div className="project-card-container">
                 {projectData.map(proj => (
-                    <a className="project-card col-xs-12" href={proj.link} target="_blank" rel="noopener noreferrer">
+
+                    <a className="project-card" href={proj.link} target="_blank" rel="noopener noreferrer">
                         <img src={`../projImg/${proj.imgSrc}`} alt="" />
                         <h3>{proj.name}</h3>
                         <p className="project-card-description">{proj.description}</p>
@@ -19,6 +20,7 @@ const Projects = () => {
                             ))}
                         </div>
                     </a>
+
                 ))}
             </div>
         </div>
